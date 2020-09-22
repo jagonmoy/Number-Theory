a71ea30326f0ce44842ccf3c4cc0b5e2ed54ec83
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2020-09-22 20:14:57 +0600

    Create Code.cpp

---
+++ Concepts of Number Theory/Sieve/General Sieve/Code.cpp
@@ -0,0 +1,18 @@
+ll  check[1000000];
+vector<ll> sieve(ll n) {
+    VL primes ;
+    memset(check,0,sizeof(check));
+    check[1] = 1 ;
+    for ( ll i = 4 ; i <= n ; i = i+2) check[i] = 1 ;    //the numbers which are not prime that means even numbers are marked by 1
+
+    for ( ll i = 3 ; i*i <= n ; i += 2) {                // Even numbers are not prime so we will check only odd numbers and increment the number by 2 to avoid even number.
+        if (!check[i]) {                                 // if  check[i] = 0 that means if the i value is a prime we will procced
+            for ( ll j = i*i ; j <= n ; j += 2*i){       // here we are starting not by j = i+i we are starting by j = i*i because the numbers less than i*i are already 
+                   check[j] = 1 ;                        // checked by the numbers less than j so no need to calculate them further . and each time we will increment the
+            }                                            // numbers by 2*i because if this is a odd number then just incrementing i it will be even and even numbers 
+        }                                                // numbers are not prime so no need to check this .
+    }   
+
+    for ( ll i = 1 ; i <= n ; i++)  if (!check[i]) primes.PB(i);   // only taking the primes numbers in vector
+    return primes ;
+}
