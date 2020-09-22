4911c907cbe7b59477f54d6e936382db65f95ce8
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2020-09-23 05:21:29 +0600

    Create SPOJ_COMDIV.cpp

---
+++ Number Theory Related Problems/Number of divisors/SPOJ_COMDIV.cpp
@@ -0,0 +1,88 @@
+Explanation :  The number of common divisors between two numbers is the number of divisors of their GCD .
+
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+#define P push
+#define INC(i,a,b) for (ll i = a; i <= b; i++)
+#define DEC(i,b,a) for (ll i = b; i >= a ; i--)
+#define inf LLONG_MAX
+#define neginf LLONG_MIN
+#define mod  1000000007
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef pair < ll , ll > PLL ;
+typedef pair < char,ll > PCL ;
+typedef deque < double > DD ;
+typedef deque < PCL > DCL ;
+typedef deque < ll > DL ;
+typedef deque < PLL > DLL ;
+typedef deque < char > DC ;
+typedef deque < string > DS ;
+typedef vector < double > VD;
+typedef vector < PCL > VCL ;
+typedef vector < ll > VL;
+typedef vector < PLL > VLL ;
+typedef vector < char > VC ;
+typedef vector < string > VS ;
+typedef map < ll ,ll > MLL ;
+typedef map < char,ll > MCL;
+typedef map < ll,char > MLC;
+typedef map < string,ll> MSL;
+typedef priority_queue < PLL > PQLL ;
+typedef priority_queue < ll > PQL ;
+typedef stack < ll > SKL ;
+typedef stack < PLL > SKLL ;
+typedef queue < ll > QL ;
+typedef queue < PLL > QLL ;
+typedef set < ll > SL ;
+typedef set < PLL > SLL ;
+typedef set < char > SC ;
+ 
+string numtostr(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll strtonum(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll GCD(ll a, ll b) {
+    if ( b == 0 ) return a ;
+    else return GCD(b,a%b);
+}
+ll LCM(ll a , ll b) {
+    ll gcd = GCD(a,b);
+     return (a/gcd)*b ;
+}
+ll divisors[1000005];
+void precalculating_number_of_divisors_1_to_n(ll n) {
+   for ( ll i = 1 ; i <= n ; i++) {
+       for ( ll j = i ; j <= n ; j += i) {
+           divisors[j]++ ;      
+       }
+   }
+} 
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    precalculating_number_of_divisors_1_to_n(1000000);
+    ll t ;
+    scanf("%lld",&t);
+    while(t--) {
+        ll a,b ;
+        scanf("%lld %lld",&a,&b);
+        printf("%lld\n",divisors[GCD(a,b)]);
+    }
+ 
+    return 0 ;
+}
