881b83eb1f59187e18a58161f2d4d459ae820a25
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2020-09-24 18:40:20 +0600

    Create Code.cpp

---
+++ Concepts of Number Theory/Euler Phi/Euler Phi for a Number N/Code.cpp
@@ -0,0 +1,12 @@
+ll euler_phi(ll n) {
+    ll result = n ;
+    for ( ll i = 2 ; i*i <= n ; i++) {
+        if ( n % i == 0 ) {
+            while(n%i == 0) n /= i ;
+            result -= result/i ;
+        }
+    }
+    if ( n > 1) result -= result/n ;
+    return result ;
+}
+ 
