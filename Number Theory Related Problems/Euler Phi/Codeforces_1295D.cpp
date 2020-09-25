9da54422062e11476856700eaecba085cc5bd23f
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2020-09-25 06:41:20 +0600

    Create Codeforces_1295D.cpp

---
+++ Number Theory Related Problems/Euler Phi/Codeforces_1295D.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+#define P push
+#define INC(i,a,b) for (ll i = a; i <= b; i++)
+#define DEC(i,b,a) for (ll i = b; i >= a ; i--)
+#define inf LLONG_MAX
+#define neginf LLONG_MIN
+#define mod  1000000007
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef pair < ll , ll > PLL ;
+typedef pair < char,ll > PCL ;
+typedef deque < double > DD ;
+typedef deque < PCL > DCL ;
+typedef deque < ll > DL ;
+typedef deque < PLL > DLL ;
+typedef deque < char > DC ;
+typedef deque < string > DS ;
+typedef vector < double > VD;
+typedef vector < PCL > VCL ;
+typedef vector < ll > VL;
+typedef vector < PLL > VLL ;
+typedef vector < char > VC ;
+typedef vector < string > VS ;
+typedef map < ll ,ll > MLL ;
+typedef map < char,ll > MCL;
+typedef map < ll,char > MLC;
+typedef map < string,ll> MSL;
+typedef priority_queue < PLL > PQLL ;
+typedef priority_queue < ll > PQL ;
+typedef stack < ll > SKL ;
+typedef stack < PLL > SKLL ;
+typedef queue < ll > QL ;
+typedef queue < PLL > QLL ;
+typedef set < ll > SL ;
+typedef set < PLL > SLL ;
+typedef set < char > SC ;
+ 
+string numtostr(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll strtonum(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll GCD(ll a, ll b) {
+    if ( b == 0 ) return a ;
+    else return GCD(b,a%b);
+}
+ll LCM(ll a , ll b) {
+    ll gcd = GCD(a,b);
+     return (a/gcd)*b ;
+}
+ll euler_phi(ll n) {
+    ll result = n ;
+    for ( ll i = 2 ; i*i <= n ; i++) {
+        if ( n % i == 0 ) {
+            while(n%i == 0) n /= i ;
+            result -= result/i ;
+        }
+    }
+    if ( n > 1) result -= result/n ;
+    return result ;
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t ;
+    cin >> t ;
+    while(t--) {
+        ll a,m ;
+        cin >> a >> m ;
+        ll gcd = GCD(a,m);
+        cout << euler_phi(m/gcd) << endl;
+    }
+ 
+    return 0 ;
+}
